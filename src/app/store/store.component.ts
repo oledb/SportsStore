bcb41d5e0bac8dbdcca1642916a687ad06904012
Андрей <zf0qwe@gmail.com>
2018-01-28 19:38:51 +0300

    added category selector and paginator

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -8,14 +8,37 @@ import { ProductRepository } from '../model/static.repository';
     templateUrl: "store.component.html"
 })
 export class StoreComponent{
+    public selectedCategory = null;
+    public productPerPage = 4;
+    public selectedPage = 1;
+
     constructor(private repository: ProductRepository){}
 
     get products(): Product[]{
-        return this.repository.getProducts();
+        let pageIndex =(this.selectedPage - 1) * this.productPerPage;
+        return this.repository.getProducts(this.selectedCategory)
+            .slice(pageIndex, pageIndex + this.productPerPage);
     }
 
     get categories(): string[]{
-        console.log(this.repository.getProducts());
         return this.repository.getCategories();
+    }
+
+    changeCategory(newCategory?: string){
+        console.log(newCategory);
+        this.selectedCategory = newCategory;
+    }
+
+    changePage(newPage: number){
+        this.selectedPage = newPage;
+    }
+
+    changePageSize(newSize: number){
+        this.productPerPage = Number(newSize);
+        this.changePage(1);
+    }
+
+    get pageNumbers(): number[] {
+        return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productPerPage)).fill(0).map((x, i) => i + 1);
     }
 }
