51f6baae403d5202aea6e70b6757066d3995c8fd
Andrey <a.kopeykin@emg.fm>
2018-01-30 17:23:38 +0300

    added guards and cartDetail html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StoreComponent } from './store/store.component';
 import { CheckoutComponent } from './store/checkout.component';
 import { CartDetailComponent } from './store/cartDetail.component';
 import { RouterModule } from '@angular/router';
+import { StoreFirstGuard } from './storeFirst.guard';
 
 
 @NgModule({
@@ -15,12 +16,16 @@ import { RouterModule } from '@angular/router';
   imports: [
     BrowserModule, StoreModule,
     RouterModule.forRoot([
-      { path: "store", component: StoreComponent },
-      { path: "cart", component: CartDetailComponent },
-      { path: "checkout", component: CheckoutComponent },
+      { path: "store", component: StoreComponent,
+    canActivate: [StoreFirstGuard] },
+      { path: "cart", component: CartDetailComponent,
+      canActivate: [StoreFirstGuard] },
+      { path: "checkout", component: CheckoutComponent,
+      canActivate: [StoreFirstGuard] },
       { path: "**", redirectTo: "/store" }
     ])
   ],
+  providers: [StoreFirstGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
