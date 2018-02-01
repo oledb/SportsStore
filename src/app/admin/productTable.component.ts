f1ee33ff65080cbb149250302c4cf40ce9a60f91
Andrey <a.kopeykin@emg.fm>
2018-02-01 18:36:14 +0300

    up pg 211

--- src/app/admin/productTable.component.ts
+++ src/app/admin/productTable.component.ts
@@ -1,6 +1,19 @@
 import { Component } from '@angular/core';
+import { Product } from "../model/Product";
+import { ProductRepository } from "../model/static.repository";
 
 @Component({
+    moduleId: module.id,
     templateUrl: 'productTable.component.html'
 })
-export class ProductTableComponent{}
+export class ProductTableComponent{
+    constructor(private repository: ProductRepository){}
+
+    getProducts(): Product[]{
+        return this.repository.getProducts();
+    }
+
+    deleteProduct(id: number){
+        this.repository.deleteProduct(id);
+    }
+}
