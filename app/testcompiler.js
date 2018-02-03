3b1e84d46450e31e171e90afbaaa0cb03c500f56
Андрей <zf0qwe@gmail.com>
2018-02-03 15:54:06 +0300

    config ts

---
+++ app/testcompiler.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Test = /** @class */ (function () {
+    function Test(message) {
+        this.message = message;
+    }
+    return Test;
+}());
+exports.Test = Test;
