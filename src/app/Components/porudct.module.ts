db8b62380253cd2bcb9d3f095fac32f646f4bf5d
Андрей <zf0qwe@gmail.com>
2018-02-11 13:59:38 +0300

    added form.model.ts (343)

--- src/app/Components/porudct.module.ts
+++ src/app/Components/porudct.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ProductComponent } from './product.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [ BrowserModule, FormsModule ],
+    imports: [ BrowserModule, FormsModule, ReactiveFormsModule ],
     exports: [ProductComponent],
     declarations: [ProductComponent]
 })
