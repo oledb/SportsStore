ab638b02ad14eae765e678d2ebcf31eecb40e546
Андрей <zf0qwe@gmail.com>
2018-02-11 15:03:50 +0300

    completed product form (350)

--- src/app/Components/form.model.ts
+++ src/app/Components/form.model.ts
@@ -9,6 +9,35 @@ export class ProductFormControl extends FormControl {
         this.label = label;
         this.modelProperty = property;
     }
+
+    getValidationMessage(): string[] {
+        let messages: string[] = [];
+        if (this.errors) {
+            for(let errorName in this.errors) {
+                if (this[errorName] === undefined) {
+                    throw new Error(`Validation type ${errorName} is undefined`);
+                }
+                messages.push(this[errorName]());
+            }
+        }
+        return messages;
+    }
+
+    private required(): string {
+        return `You must enter a ${this.label}`;
+    }
+
+    private minlength(): string {
+        return `A ${this.label} must be at least ${this.errors['minlength'].requiredLength}`;
+    }
+
+    private maxlength(): string {
+        return `A ${this.label} must be no more than ${this.errors['maxlength'].requiredLength}`;
+    }
+
+    private pattern(): string {
+        return `The ${this.label} contains illegal character`;
+    }
 }
 
 export class ProductFormGroup extends FormGroup {
@@ -31,5 +60,12 @@ export class ProductFormGroup extends FormGroup {
     get productControls(): ProductFormControl[] {
         return Object.keys(this.controls)
             .map(k => this.controls[k] as ProductFormControl);
+    }
+
+    getFormValidationMessages(form: any): string[] {
+        let messages: string[] = [];
+        this.productControls.forEach(c => c.getValidationMessage()
+            .forEach(m => messages.push(m)));
+        return messages;
     }
 }
--- src/app/Components/product.component.ts
+++ src/app/Components/product.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Model } from '../model/repository.model';
 import { Product } from '../model/product.model';
+import { ProductFormGroup } from "./form.model";
 
 @Component({
     selector: 'app-products',
@@ -12,6 +13,7 @@ export class ProductComponent {
 
     model: Model = new Model();
     selectedId: number;
+    form: ProductFormGroup = new ProductFormGroup();
 
     newProduct = new Product();
 
@@ -34,40 +36,41 @@ export class ProductComponent {
     get jsonProduct() {
         return JSON.stringify(this.newProduct);
     }
-
+    formSubmitted: boolean = false;
     addProduct(form: NgForm) {
+        this.formSubmitted = true;
         if (form.valid){
             console.log('New Product' + this.jsonProduct);
             this.newProduct = new Product();
             form.reset();
+            this.formSubmitted = false;
         }
-        
     }
 
-    getValidationMessages(state: any, thingName?: string): string[] {
-        const thing: string = state.path || thingName;
-        const message: string[] = [];
-        if (state.errors) {
-            for (const errorName in state.errors) {
-                if (state.errors.hasOwnProperty(errorName)) {
-                    switch (errorName) {
-                        case 'required':
-                            message.push(`You must enter a ${thing}`);
-                            break;
-                        case 'minlength':
-                            message.push(`A ${thing} must be at least ${state.errors['minlength'].requiredLength}`);
-                            break;
-                        case 'pattern':
-                            message.push(`The ${thing} contains illegal character`);
-                            break;
-                        default:
-                            throw new Error(`Unkown property ${errorName}`);
-                    }
-                } else {
-                    throw new Error(`No property ${errorName}`);
-                }
-            }
-        }
-        return message;
-    }
+    // getValidationMessages(state: any, thingName?: string): string[] {
+    //     const thing: string = state.path || thingName;
+    //     const message: string[] = [];
+    //     if (state.errors) {
+    //         for (const errorName in state.errors) {
+    //             if (state.errors.hasOwnProperty(errorName)) {
+    //                 switch (errorName) {
+    //                     case 'required':
+    //                         message.push(`You must enter a ${thing}`);
+    //                         break;
+    //                     case 'minlength':
+    //                         message.push(`A ${thing} must be at least ${state.errors['minlength'].requiredLength}`);
+    //                         break;
+    //                     case 'pattern':
+    //                         message.push(`The ${thing} contains illegal character`);
+    //                         break;
+    //                     default:
+    //                         throw new Error(`Unkown property ${errorName}`);
+    //                 }
+    //             } else {
+    //                 throw new Error(`No property ${errorName}`);
+    //             }
+    //         }
+    //     }
+    //     return message;
+    // }
 }
