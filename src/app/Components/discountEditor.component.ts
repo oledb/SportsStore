7b284d846f4e4b4209b9bc161683884907c708cc
Андрей <zf0qwe@gmail.com>
2018-02-13 22:05:14 +0300

    added discount

---
+++ src/app/Components/discountEditor.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from "@angular/core";
+import { DiscountService } from "./discount.service"
+
+@Component({
+    selector: "paDiscountEditor",
+    template: ``
+})
+export class PaDiscountEditor{
+    
+}
